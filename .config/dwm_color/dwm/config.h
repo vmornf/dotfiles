3f5bd0c2d16af1269da4a4363d1120b4694b8e3a
Jonas Ornfelt <vmornf@gmail.com>
2022-06-23 21:32:49 +0200

    Update to dwm color

--- .config/dwm_color/dwm/config.h
+++ .config/dwm_color/dwm/config.h
@@ -106,14 +106,10 @@ static const Layout layouts[] = {
 #define STACKKEYS(MOD,ACTION) \
 	{ MOD,	XK_j,	ACTION##stack,	{.i = INC(+1) } }, \
 	{ MOD,	XK_k,	ACTION##stack,	{.i = INC(-1) } }, \
+	{ MOD|ControlMask,	XK_j,	ACTION##stack,	{.i = -1 } }, \
+	{ MOD|ControlMask,	XK_k,	ACTION##stack,	{.i = 0 } }, \
 	/* { MOD,	XK_h,	ACTION##stack,	{.i = INC(+1) } }, \ */
 	/* { MOD,	XK_l,	ACTION##stack,	{.i = INC(-1) } }, \ */
-
-	/* { MOD,  XK_v,   ACTION##stack,  {.i = 0 } }, \ */
-	/* { MOD, XK_grave, ACTION##stack, {.i = PREVSEL } }, \ */
-	/* { MOD, XK_a,     ACTION##stack, {.i = 1 } }, \ */
-	/* { MOD, XK_z,     ACTION##stack, {.i = 2 } }, \ */
-	/* { MOD, XK_x,     ACTION##stack, {.i = -1 } }, */
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -176,7 +172,6 @@ static Key keys[] = {
 	{ MODKEY,			XK_Tab,		view,		{0} },
 	{ MODKEY,			XK_Tab,		view,		{0} },
 	{ MODKEY,			XK_q,		killclient,	{0} },
-	/* { MODKEY|ShiftMask,             XK_q,      	quit,           {0} }, */
 	{ MODKEY|ShiftMask,			XK_x,		spawn,		SHCMD("i3lock-fancy") },
 	{ MODKEY|ControlMask,       XK_x,       spawn,      SHCMD("i3lock -i ~/Downloads/lock-wallpaper.png")},
 	
