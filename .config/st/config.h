beebb2896bd048f8e58aac63639d54909321482f
Jonas Ornfelt <vmornf@gmail.com>
2022-06-27 14:21:11 +0200

    St border change

--- .config/st/config.h
+++ .config/st/config.h
@@ -8,7 +8,7 @@
 static char *font = "JetBrainsMono Nerd Font:size=11:style=bold:antialias=true:autohint=true";
 /* static char *font = "mono:pixelsize=12:antialias=true:autohint=true"; */
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
-static int borderpx = 2;
+static int borderpx = 16;
 
 /*
  * What program is execed by st depends of these precedence rules:
